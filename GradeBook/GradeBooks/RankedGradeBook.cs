51a904e3d64161d50c59e6b4012d04bbd09a2ba3
Harry Ammon <harry_a99@hotmail.co.uk>
2019-08-19 13:01:10 +0100

    Override statistics

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -28,31 +28,27 @@ namespace GradeBook.GradeBooks
             for(int i = 0; i < numberOfStudents; i++)
             {
                 if(Students[i].AverageGrade >= averageGrade)
-                {
                     studentsWithHigherGrade++; ;
-                }
             }
 
             if (studentsWithHigherGrade <= twentyPercentOfNumberOfStudents)
-            {
                 return 'A';
-            }
             else if (studentsWithHigherGrade <= twentyPercentOfNumberOfStudents * 2)
-            {
                 return 'B';
-            }
             else if (studentsWithHigherGrade <= twentyPercentOfNumberOfStudents * 3)
-            {
                 return 'C';
-            }
             else if (studentsWithHigherGrade <= twentyPercentOfNumberOfStudents * 4)
-            {
                 return 'D';
-            }
             else
-            {
                 return 'F';
-            }
+        }
+
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+            else
+                base.CalculateStatistics();
         }
     }
 }
