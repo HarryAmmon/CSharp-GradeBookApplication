f1c358d0b23328c42bb086ae3a253cfd15c3add3
Harry Ammon <harry_a99@hotmail.co.uk>
2019-08-19 12:19:24 +0100

    Overrode RankedGradeBook's GetLetterGrade

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,12 +1,58 @@
 ﻿using GradeBook.Enums;
+using System;
 
 namespace GradeBook.GradeBooks
 {
+    // This entire class was added by Harry
     public class RankedGradeBook : BaseGradeBook
     {
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            double numberOfStudents = Students.Count;
+
+            if (numberOfStudents < 5)
+            {
+                throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
+            }
+            //Array.Sort();
+            //Students.Reverse();
+;
+            double twentyPercentOfNumberOfStudents = Math.Round(numberOfStudents / 5d);
+
+            int studentsWithHigherGrade = 0;
+            for(int i = 0; i < numberOfStudents; i++)
+            {
+                if(Students[i].AverageGrade >= averageGrade)
+                {
+                    studentsWithHigherGrade++; ;
+                }
+            }
+
+            if (studentsWithHigherGrade <= twentyPercentOfNumberOfStudents)
+            {
+                return 'A';
+            }
+            else if (studentsWithHigherGrade <= twentyPercentOfNumberOfStudents * 2)
+            {
+                return 'B';
+            }
+            else if (studentsWithHigherGrade <= twentyPercentOfNumberOfStudents * 3)
+            {
+                return 'C';
+            }
+            else if (studentsWithHigherGrade <= twentyPercentOfNumberOfStudents * 4)
+            {
+                return 'D';
+            }
+            else
+            {
+                return 'F';
+            }
         }
     }
 }
